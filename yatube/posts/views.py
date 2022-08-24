b50f1049fa054624d2d550ea518bee2d3b81450b
Алексей Моисеев <saffqa1@yandex.ru>
2022-08-24 16:48:01 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Добрый день! Вы находитесь на главной странице новой социальной'
+                        'сети "Yatube"')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Перед Вами представлен список постов, отсортированных по группам: {slug}')
