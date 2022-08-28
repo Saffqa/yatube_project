e8df1fa066ae1727e56ecf4eae498ea40a157131
Алексей Моисеев <saffqa1@yandex.ru>
2022-08-28 22:41:40 +0300

    new

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def index(request):
     template = 'posts/index.html'
     return render(request, template)
 
-def group_posts(request):
+
+def group_posts(request, slug):
     template = 'posts/group_list.html'
     return render(request, template)
+    #HttpResponse('Список posts')
+
+
+#def group_posts(request, slug):
+    return HttpResponse('Список posts')
+
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+#def ice_cream_detail(request, pk):
+#    return HttpResponse(f'Мороженое номер {pk}')
