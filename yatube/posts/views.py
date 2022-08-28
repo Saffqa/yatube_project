9e6bc0302caae64a2092997361964f318e535e61
Алексей Моисеев <saffqa1@yandex.ru>
2022-08-28 15:19:29 +0300

    new

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Добрый день! Вы находитесь на главной странице новой социальной'
-                        'сети "Yatube"')
+    template = 'posts/index.html'
+    return render(request, template)
 
-def group_posts(request, slug):
-    return HttpResponse(f'Перед Вами представлен список постов, отсортированных по группам: {slug}')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
